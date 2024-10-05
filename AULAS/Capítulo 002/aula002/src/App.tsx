5bea88155d03e8b9c82ff163d6732383e86a1011
Diego Melo <diegohenriquemelo14@gmail.com>
2024-10-05 16:26:16 -0300

    Learn React.js

---
+++ AULAS/Capítulo 002/aula002/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Card} from "./components/card"
+
+function App() {
+  return (
+   <div>
+    Hello world
+      <Card />
+      <Card />
+      <Card />
+   </div>
+  );
+}
+
+export default App;
---
+++ AULAS/Capítulo 002/aula002/src/components/card.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Card = () => {
+  return (
+    <div>
+        <h1>Card</h1>
+        <p>paragrafo</p>
+    </div>
+  );
+};
